83e9037befd8717b5cf2dde5d440c21751d4a43c
Daniel King'ara <murigukingara@gmail.com>
2025-07-20 23:18:15 +0300

    Multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number to see its multiplication table: "))
+
+iterator = 1,2,3,4,5,6,7,8,9,10,
+
+for iterator in iterator:
+ result = number * iterator
+ print(f"{number} * {iterator} = {result}")
