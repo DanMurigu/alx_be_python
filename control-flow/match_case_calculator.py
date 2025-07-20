05a92aaeeaabc644eca5125fe91c2faf11720b26
Daniel King'ara <murigukingara@gmail.com>
2025-07-20 23:38:41 +0300

    Drawing patteerns

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,7 +3,7 @@ num2 = "num2"
 operation = "(+, -, *, /)"
 
 num1 = int(input("Enter the first number: " ))
-num2 = int(input("Enter the second number: " ))
+num2 = int(input("Enter the second number: gi" ))
 operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
