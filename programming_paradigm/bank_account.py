ea84af6a67876c76d0b71b6c6e7182b706494153
Daniel King'ara <murigukingara@gmail.com>
2025-08-08 06:44:31 +0300

    Bank Account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -32,8 +32,8 @@ class BankAccount:
         return True
     
     def display_balance(self):
-        print(f"Current Balance: {self.account_balance: }")
-        
+        print(f"Current Balance: ${self.account_balance: }")
+
     
     
 
